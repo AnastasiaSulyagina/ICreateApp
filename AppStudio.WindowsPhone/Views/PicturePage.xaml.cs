7b7fa2ee63849d48378a28fb9813ac600307d842
Anastasia Sulyagina <anastasia.sulyagina@gmail.com>
2015-05-13 16:20:46 +0300

    Some data views and connections added

---
+++ AppStudio.WindowsPhone/Views/PicturePage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace AppStudio.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PicturePage : Page
+    {
+        private FileOpenPicker fileOpenPicker = new FileOpenPicker();
+        private CoreApplicationView view;
+        private String ImagePath;
+
+        public PicturePage()
+        {
+            this.InitializeComponent();
+            //ProfilePicture.Source = new BitmapImage(new Uri(CurrentUser.PictureUrl));
+            view = CoreApplication.GetCurrentView();
+
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void ChangePictureButton_Click(object sender, RoutedEventArgs e)
+        {
+            ImagePath = string.Empty;
+            FileOpenPicker filePicker = new FileOpenPicker();
+            filePicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            filePicker.ViewMode = PickerViewMode.Thumbnail;
+
+            // Filter to include a sample subset of file types
+            filePicker.FileTypeFilter.Clear();
+            filePicker.FileTypeFilter.Add(".bmp");
+            filePicker.FileTypeFilter.Add(".png");
+            filePicker.FileTypeFilter.Add(".jpeg");
+            filePicker.FileTypeFilter.Add(".jpg");
+
+            filePicker.PickSingleFileAndContinue();
+            view.Activated += viewActivated;
+
+        }
+        private async void viewActivated(CoreApplicationView sender, IActivatedEventArgs args1)
+        {
+            FileOpenPickerContinuationEventArgs args = args1 as FileOpenPickerContinuationEventArgs;
+
+            if (args != null)
+            {
+                if (args.Files.Count == 0) return;
+
+                view.Activated -= viewActivated;
+                StorageFile storageFile = args.Files[0];
+                var stream = await storageFile.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                var bitmapImage = new Windows.UI.Xaml.Media.Imaging.BitmapImage();
+                await bitmapImage.SetSourceAsync(stream);
+
+                var decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(stream);
+                ProfilePicture.Source = bitmapImage;
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            ServerAPI.UploadPicture();
+        }
+    }
+}
